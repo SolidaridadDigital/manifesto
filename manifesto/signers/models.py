bf1c5ef4cb741069eb1112a508883e0d74250bd2
Mauricio <mgiadach@gmail.com>
2012-06-08 13:13:01 -0400

    Modificado formulario, agregado SignerProfile

---
+++ manifesto/signers/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+class SignerProfile(models.Model):
+  user = models.OneToOneField(User)
+  comment = models.TextField(blank=True)
+  country = models.IntegerField(blank=True)
+  suscribed = models.BooleanField(default=False)
+  
+  def __unicode__(self):
+    return self.user.first_name
+
+def create_user_profile(sender, instance, created, **kwargs):
+  if created:
+    SignerProfile.objects.create(user=instance)
+    
+post_save.connect(create_user_profile, sender=User)
