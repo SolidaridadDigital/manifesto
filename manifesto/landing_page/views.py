08ece96d3325a1605b0f0d8207f18ee560b66943
npinilla <npinillac@gmail.com>
2012-07-04 16:26:18 -0400

    arreglado error de identacion

--- manifesto/landing_page/views.py
+++ manifesto/landing_page/views.py
@@ -11,7 +11,7 @@ import csv
 
 def index(request):
     # se abre el archivo con los datos de los paises
-        with open('/home/chileagil/manifesto/app/utilidades/iso3166.csv', 'rb') as file_countries:
+    with open('/home/chileagil/manifesto/app/utilidades/iso3166.csv', 'rb') as file_countries:
         countries = [(row['Code'], row['English']) for row in csv.DictReader(file_countries)] # se obtiene el codigo y el nombre del pais en ingles
     for country in countries:
         countries2.append(country[1]) # se agregan los nombres de los paises en la lista
