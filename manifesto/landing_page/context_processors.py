4b38b1d9063ece3f18cc88d9c8281082c8fe9b14
npinilla <npinillac@gmail.com>
2012-07-05 02:13:53 -0400

    modificado nombre last_three_signers por last_signers

--- manifesto/landing_page/context_processors.py
+++ manifesto/landing_page/context_processors.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 
 # envia los ultimos 15 firmantes en el request.context para que esten disponibles en todas las paginas
-def last_three_signers(request):
+def last_signers(request):
     users = User.objects.filter(is_active=True).order_by('-date_joined')[:15]
     return {'users': users}
--- manifesto/manifesto/settings_default.py
+++ manifesto/manifesto/settings_default.py
@@ -133,7 +133,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
    "django.core.context_processors.static",
    "django.core.context_processors.tz",
    "django.contrib.messages.context_processors.messages",
-   "landing_page.context_processors.last_three_signers",
+   "landing_page.context_processors.last_signers",
    )
 
 LANGUAGES = (
