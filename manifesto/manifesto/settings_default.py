a98893dbe1fe1eaf39151cb18470b5172c38026f
npinilla <npinillac@gmail.com>
2012-07-04 22:28:50 -0400

    holamundo sacado del settings_default.py

--- manifesto/manifesto/settings_default.py
+++ manifesto/manifesto/settings_default.py
@@ -156,7 +156,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'holamundo',
     'landing_page',
     'signers',
     # Para allauth:
